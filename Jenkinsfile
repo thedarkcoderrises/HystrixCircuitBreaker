1831786898cdba92a8e391d0e5e608a71610b79c
thedarkcoderrises <abhikrish88@gmail.com>
2019-02-23 17:21:13 +0530

    cloudwatch aws logging

--- Jenkinsfile
+++ Jenkinsfile
@@ -36,6 +36,12 @@ pipeline {
             agent any
              steps {
                      sh 'docker run -d -p 8081:8080 -v /home/ec2-user/myDocker/springboot-hystrix/localmount:/tmp --log-driver json-file --log-opt max-size=20k --log-opt max-file=3 --name springboot-hystrix springboot-hystrix:1.0'
+                     script{
+                        containerId = sh (  script :'docker ps -aqf "name=springboot-hystrix"',
+                                            returnStdout: true
+                                         ).trim()
+                     }
+                     sh 'docker logs containerId > /home/ec2-user/myDocker/springboot-hystrix/localmount/logs/hystrix.log'
                 }
         }
 
--- src/main/java/com/circuitbreaker/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/circuitbreaker/service/impl/GreetingServiceImpl.java
@@ -2,17 +2,23 @@ package com.circuitbreaker.service.impl;
 
 import com.circuitbreaker.service.GreetingService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
+
+    private static Logger LOG = LoggerFactory.getLogger(GreetingServiceImpl.class);
     @Override
     @HystrixCommand(fallbackMethod = "defaultGreeting")
     public String greeting(String username) {
         if("cb".equalsIgnoreCase(username)){
             throw new NullPointerException();
         }
-        return String.format("Hello %s Welcome to Docker CI\\CD!", username.toUpperCase());
+        String info = String.format("Hello %s Welcome to Docker CI\\CD!", username.toUpperCase());
+        LOG.info(info);
+        return info;
     }
 
     private String defaultGreeting(String username) {
