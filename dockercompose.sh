a14ab744a3bc3cf0f6dbf15f0722e71cd643067f
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 00:12:11 +0530

    https2

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,2 +1,1 @@
 docker run -d -p 8081:8081 -p 8443:8443 -v $PWD/logs:/logs -v $PWD/hystrix/localmount:/tmp --log-driver json-file --log-opt max-size=20k --log-opt max-file=3 --name hystrix hystrix:1.0
-
