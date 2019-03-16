5f7f12c848d56ea29c53822c6e0cf2ad6ec6dc75
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 23:41:49 +0530

    test

--- src/main/java/com/circuitbreaker/config/AppConfig.java
+++ src/main/java/com/circuitbreaker/config/AppConfig.java
@@ -12,52 +12,52 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
-//@Configuration
+@Configuration
 public class AppConfig {
 
-//
-//    @Value("${http.port}")
-//    private int httpPort;
-//
-//    @Value("${server.port}")
-//    private int httpsPort;
-//
-//    private static Logger LOG = LoggerFactory.getLogger(AppConfig.class);
-//
-//    @Bean
-//   public TomcatEmbeddedServletContainerFactory containerFactory () throws Exception {
-//        TomcatEmbeddedServletContainerFactory containerFactory = new TomcatEmbeddedServletContainerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        containerFactory.addAdditionalTomcatConnectors(createConnection());
-//
-//        return containerFactory;
-//    }
-//
-//    private Connector createConnection() throws Exception {
-//        verifyPorts();
-//        final String protocol = "org.apache.coyote.http11.Http11NioProtocol";
-//        final Connector connector = new Connector(protocol);
-//        connector.setScheme("http");
-//        connector.setPort(httpPort);
-//        connector.setRedirectPort(httpsPort);
-//        return connector;
-//    }
-//
-//    private void verifyPorts() throws Exception {
-//        if (StringUtils.isEmpty(httpPort) || StringUtils.isEmpty(httpsPort))
-//            throw new Exception("Inavlid port");
-//        else
-//            LOG.info("http.port: {}, https.port: {}",httpPort,httpsPort);
-//    }
+
+    @Value("${http.port}")
+    private int httpPort;
+
+    @Value("${server.port}")
+    private int httpsPort;
+
+    private static Logger LOG = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+   public TomcatEmbeddedServletContainerFactory containerFactory () throws Exception {
+        TomcatEmbeddedServletContainerFactory containerFactory = new TomcatEmbeddedServletContainerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        containerFactory.addAdditionalTomcatConnectors(createConnection());
+
+        return containerFactory;
+    }
+
+    private Connector createConnection() throws Exception {
+        verifyPorts();
+        final String protocol = "org.apache.coyote.http11.Http11NioProtocol";
+        final Connector connector = new Connector(protocol);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
+
+    private void verifyPorts() throws Exception {
+        if (StringUtils.isEmpty(httpPort) || StringUtils.isEmpty(httpsPort))
+            throw new Exception("Inavlid port");
+        else
+            LOG.info("http.port: {}, https.port: {}",httpPort,httpsPort);
+    }
 
 
 }
