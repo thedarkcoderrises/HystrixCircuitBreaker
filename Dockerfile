4d7cdd70d353fd69b835ffa00ebe0afa049e8de7
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 20:33:44 +0530

    docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM java:8
 VOLUME /tmp
 ADD target/HystrixCB-1.0-SNAPSHOT.jar app.jar
 EXPOSE 8081
-EXPOSE 8443
+#EXPOSE 8443
 ENTRYPOINT ["java","-jar","/app.jar"]
--- dockercompose.sh
+++ dockercompose.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8081:8081 -p 8443:8443 -v $PWD/logs/H1:/logs -e http.port:8081 -e server.port:8443 --name hystrix hystrix:1.0
+docker run -d -p 9081:8081 -v $PWD/logs/H1:/logs --name hystrix hystrix:1.0
