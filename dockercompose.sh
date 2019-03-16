6502e84430a2d652a0994e53f944c8d11c834a7d
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 11:51:22 +0530

    nginx

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8081:8081 -p 8443:8443 -v $PWD/logs:/logs -v $PWD/hystrix/localmount:/tmp --log-driver json-file --log-opt max-size=20k --log-opt max-file=3 --name hystrix hystrix:1.0
+docker run -d -p 9081:80 -p 9443:443 -v $PWD/H1/logs:/logs --log-driver json-file --log-opt max-size=20k --log-opt max-file=3 --name hystrix1 hystrix:1.0
