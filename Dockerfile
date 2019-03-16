48eca64bf80e68b402aee46afa211720921caf99
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 20:41:23 +0530

    override port

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@
 FROM java:8
 VOLUME /tmp
 ADD target/HystrixCB-1.0-SNAPSHOT.jar app.jar
-EXPOSE 8081
-#EXPOSE 8443
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- dockercompose.sh
+++ dockercompose.sh
@@ -1,1 +1,1 @@
-docker run -d -p 9081:8081 -v $PWD/logs/H1:/logs --name hystrix hystrix:1.0
+docker run -d -p 9080:8080 -v $PWD/logs/H1:/logs --name hystrix hystrix:1.0
