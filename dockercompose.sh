a59cb02197ced847deb7b1e19b380291ee0948e4
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-16 20:11:02 +0530

    revert ssl

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,1 +1,1 @@
-docker run -p 9081:8080 -d -v $PWD/logs/H1:/logs -e http.port=9081 -e server.port=8443 --name hystrix1 hystrix:1.0
+docker run -p 9081:8080 -d -v $PWD/logs/H1:/logs --name hystrix hystrix:1.0
