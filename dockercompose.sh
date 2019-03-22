134ed4105a887c53c3bbd3c03bfb6ece0f05ae5a
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-22 22:48:48 +0530

    nginx

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,2 +1,2 @@
-docker run -p 9443:8443 -d -v $PWD/logs/H1:/logs --name hystrix1 hystrix:1.0
-docker run -p 9444:8443 -d -v $PWD/logs/H1:/logs --name hystrix2 hystrix:1.0
+docker run -p 9443:8443 -d -v /home/ec2-user/logs/logs/H1:/logs --name hystrix1 hystrix:1.0
+docker run -p 9444:8443 -d -v /home/ec2-user/logs/logs/H2:/logs --name hystrix2 hystrix:1.0
